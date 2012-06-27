c882814af4f21c47e696e0774bb8e4a026ddacb8
Andriy Zhdanov <azhdanov@gmail.com>
2012-06-27 23:47:38 +0200

    read csv

--- app/models/License.rb
+++ app/models/License.rb
@@ -1,2 +1,8 @@
-class License < ActiveRecord::Base
+require 'csv'    
+
+class License 
+  def self.find ignore
+    csv_text = File.read('licenseReport.csv')
+    csv = CSV.parse(csv_text, :headers => true)
+  end
 end
