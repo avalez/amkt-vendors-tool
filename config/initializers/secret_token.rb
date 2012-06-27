3ea5e5dbbc779f434080c49eac4cd355e62e1d1c
Andriy Zhdanov <azhdanov@gmail.com>
2012-06-27 22:25:29 +0200

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmktClient::Application.config.secret_token = '37a41c44bd048d21b952c80fafe9bd42d4d9651f8b6c7c6ba3e78039fe5a298f02dee67cc36e3a5ea338d626b7be48f3c691a5a91ba760492729d64410fafa36'
