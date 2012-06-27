b1b6c1b17330b8429f5cfdcd5a4ab2a58154cfe6
Andriy Zhdanov <azhdanov@gmail.com>
2012-06-28 00:16:38 +0200

    initial pivot view

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -2,4 +2,15 @@ class LicensesController < ApplicationController
   def index
     @licenses = License.find :all
   end
+
+  def pivot
+    @licenses = License.find(:all).reduce({}) do |m, row|
+      startDate = row['startDate']
+      record = m[startDate] || {}
+      edition = row['edition']
+      record[edition] = 1 + (record[edition] || 0)
+      m[startDate] = record
+      m
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ AmktClient::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # maps HTTP verbs to controller actions automatically:
-  resources :licenses
+  resources :licenses do
+    collection do
+      get 'pivot'
+    end
+  end
 
   # Sample resource route with options:
   #   resources :products do
