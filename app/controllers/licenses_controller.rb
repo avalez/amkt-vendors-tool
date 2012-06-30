371c27f2f1b0eab4cce71cb6ab111230ad06d84f
Andriy Zhdanov <azhdanov@gmail.com>
2012-07-01 01:08:49 +0200

    all license editions

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -4,12 +4,15 @@ class LicensesController < ApplicationController
   end
 
   def pivot
+    @all_editions = License.all_editions
+    @total = Hash[@all_editions.map {|edition, price| [edition, 0]}]
     @licenses = License.find(:all).reduce({}) do |m, row|
       startDate = row['startDate']
       record = m[startDate] || {}
       edition = row['edition']
       record[edition] = 1 + (record[edition] || 0)
       m[startDate] = record
+      @total[edition] += 1
       m
     end
   end
--- app/models/License.rb
+++ app/models/License.rb
@@ -1,6 +1,11 @@
 require 'csv'    
 
 class License 
+  def self.all_editions
+    {'Evaluation' => 0, '10 Users' => 10, '25 Users' => 10, '50 Users' => 50, '100 Users' => 50,
+     '500 Users' => 50, 'Enterprise 10000+ Users' => 100, 'Unlimited Users' => 100}
+  end
+
   def self.find ignore
     csv_text = File.read('licenseReport.csv')
     csv = CSV.parse(csv_text, :headers => true)
