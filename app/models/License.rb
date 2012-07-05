f4ad65126d83914a2ba8b16377be1359427d002d
Andriy Zhdanov <azhdanov@gmail.com>
2012-07-05 13:53:47 +0200

    fix reduced prices

--- app/models/License.rb
+++ app/models/License.rb
@@ -5,14 +5,22 @@ class License
     {'Evaluation' => 0, '10 Users' => 10, '25 Users' => 10, '50 Users' => 50, '100 Users' => 50,
      '500 Users' => 50, 'Enterprise 10000+ Users' => 100, 'Unlimited Users' => 100}
 
-  @@commercial_renewal = @@commercial_purchase.reduce({}) { |m, edition, price| m[edition] = (price.to_f / 2).ceil; m }
+  @@commercial_renewal = @@commercial_purchase.reduce({}) { |m, (edition, price)| m[edition] = (price.to_f / 2).ceil; m }
 
   @@academic_purchase = @@commercial_renewal.update Hash['10 Users' => 10]
 
-  @@academic_renewal = @@commercial_purchase.reduce({}) { |m, edition, price| m[edition] = (price.to_f / 2).ceil; m }
+  @@academic_renewal = @@academic_purchase.reduce({}) { |m, (edition, price)| m[edition] = (price.to_f / 2).ceil; m }
 
   def self.all_editions
     @@commercial_purchase
+  end
+
+  @@commercial_licenseTypes =  ['Commercial', 'Starter', 'Open Source']
+
+  @@paid_licenseTypes =  @@commercial_licenseTypes << 'Academic'
+
+  def self.paid_licenseTypes
+    @@paid_licenseTypes
   end
 
   def self.price license
