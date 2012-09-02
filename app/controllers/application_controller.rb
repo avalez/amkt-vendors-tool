1ce6040b8fed71aca2470eb547d73145a0264c26
Andriy Zhdanov <azhdanov@gmail.com>
2012-09-02 14:41:58 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'password'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
@@ -7,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     Rails.env != 'production' || authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['amkt_client_username'] && Pssword.check(password, ENV['amkt_client_password'])
+      username == ENV['amkt_client_username'] && Password.check(password, ENV['amkt_client_password'])
     end
   end
 end
